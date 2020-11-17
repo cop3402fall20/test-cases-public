5ff50cf26521a3165c2c843002d36d267ad29f48
kaisil <kaisil@protonmail.com>
2020-11-17 14:54:16 -0500

    Update to match removal

--- proj3/functioncall.s
+++ proj3/functioncall.s
@@ -29,31 +29,14 @@ do:
 	# emit the function prologue
 	push	%rbp
 	mov	%rsp, %rbp
-	sub	$24, %rsp
+	sub	$8, %rsp
 	push	%rbx
 	# move parameter onto the stack
 	mov	%rdi, -8(%rbp)
 	# generate code for the body
-	# generate code for the right-hand side of the assignment
+	# generate code for the return expression
 	# push the integer
 	mov	$100, %rax
-	push	%rax
-	pop	%rax
-	mov	%rax, -24(%rbp)
-	# generate code for the right-hand side of the assignment
-	# evaluate the parameter
-	mov	-24(%rbp), %rax
-	push	%rax
-	# pass the parameter
-	pop	%rdi
-	# call the function
-	call	fi
-	# push the return value
-	push	%rax
-	pop	%rax
-	mov	%rax, -16(%rbp)
-	# generate code for the return expression
-	mov	-16(%rbp), %rax
 	push	%rax
 	# save the return expression into %rax per the abi
 	pop	%rax
@@ -69,43 +52,37 @@ go:
 	# emit the function prologue
 	push	%rbp
 	mov	%rsp, %rbp
-	sub	$24, %rsp
+	sub	$8, %rsp
 	push	%rbx
 	# move parameter onto the stack
 	mov	%rdi, -8(%rbp)
 	# generate code for the body
-	# generate code for the right-hand side of the assignment
+	# generate code for the return expression
+	# push the integer
+	mov	$42, %rax
+	push	%rax
+	# save the return expression into %rax per the abi
+	pop	%rax
+	# emit the epilogue
+	pop	%rbx
+	mov	%rbp, %rsp
+	pop	%rbp
+	ret
+.text
+.globl no
+.type no, @function
+no:
+	# emit the function prologue
+	push	%rbp
+	mov	%rsp, %rbp
+	sub	$8, %rsp
+	push	%rbx
+	# move parameter onto the stack
+	mov	%rdi, -8(%rbp)
+	# generate code for the body
+	# generate code for the return expression
 	# push the integer
 	mov	$0, %rax
-	push	%rax
-	pop	%rax
-	mov	%rax, -24(%rbp)
-	# generate code for the right-hand side of the assignment
-	# evaluate the parameter
-	mov	-24(%rbp), %rax
-	push	%rax
-	# pass the parameter
-	pop	%rdi
-	# call the function
-	call	do
-	# push the return value
-	push	%rax
-	pop	%rax
-	mov	%rax, -16(%rbp)
-	# generate code for the return expression
-	# generate code for the left operand
-	mov	-8(%rbp), %rax
-	push	%rax
-	# generate code for the right operand
-	mov	-16(%rbp), %rax
-	push	%rax
-	# pop the right operand
-	pop	%rbx
-	# pop the left operand
-	pop	%rax
-	# do the subtraction
-	sub	%rbx, %rax
-	# push the expression result
 	push	%rax
 	# save the return expression into %rax per the abi
 	pop	%rax
@@ -122,19 +99,149 @@ main:
 	# emit main's prologue
 	push	%rbp
 	mov	%rsp, %rbp
-	sub	$32, %rsp
+	sub	$48, %rsp
 	push	%rbx
 	# move argc and argv from parameter registers to the stack
-	mov	%rdi, -24(%rbp)
-	mov	%rsi, -32(%rbp)
+	mov	%rdi, -40(%rbp)
+	mov	%rsi, -48(%rbp)
 	# generate code for the body
 	# generate code for the right-hand side of the assignment
-	# push the integer
-	mov	$10, %rax
+	# evaluate the parameter
+	mov	-8(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	fi
+	# push the return value
+	push	%rax
+	pop	%rax
+	mov	%rax, -16(%rbp)
+	# generate code for the right-hand side of the assignment
+	# evaluate the parameter
+	mov	-16(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	do
+	# push the return value
+	push	%rax
+	pop	%rax
+	mov	%rax, -24(%rbp)
+	# generate code for the right-hand side of the assignment
+	# evaluate the parameter
+	mov	-24(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	go
+	# push the return value
+	push	%rax
+	pop	%rax
+	mov	%rax, -32(%rbp)
+	# generate code for the right-hand side of the assignment
+	# generate code for the left operand
+	# generate code for the left operand
+	# evaluate the parameter
+	mov	-16(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	fi
+	# push the return value
+	push	%rax
+	# generate code for the right operand
+	# evaluate the parameter
+	mov	-24(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	do
+	# push the return value
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the addition
+	add	%rbx, %rax
+	# push the expression result
+	push	%rax
+	# generate code for the right operand
+	# generate code for the left operand
+	# evaluate the parameter
+	mov	-16(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	do
+	# push the return value
+	push	%rax
+	# generate code for the right operand
+	# evaluate the parameter
+	mov	-32(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	go
+	# push the return value
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the subtraction
+	sub	%rbx, %rax
+	# push the expression result
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the division
+	cdq
+	idiv	%rbx
+	# push the expression result
 	push	%rax
 	pop	%rax
 	mov	%rax, -8(%rbp)
 	# generate code for the right-hand side of the assignment
+	# generate code for the left operand
+	# generate code for the left operand
+	# evaluate the parameter
+	mov	-16(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	go
+	# push the return value
+	push	%rax
+	# generate code for the right operand
+	# evaluate the parameter
+	mov	-24(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	go
+	# push the return value
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the multiplication
+	imul	%rbx, %rax
+	# push the expression result
+	push	%rax
+	# generate code for the right operand
 	# evaluate the parameter
 	mov	-8(%rbp), %rax
 	push	%rax
@@ -144,8 +251,283 @@ main:
 	call	go
 	# push the return value
 	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the division
+	cdq
+	idiv	%rbx
+	# push the expression result
+	push	%rax
 	pop	%rax
 	mov	%rax, -16(%rbp)
+	# generate code for the right-hand side of the assignment
+	# generate code for the left operand
+	# generate code for the left operand
+	# evaluate the parameter
+	mov	-16(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	go
+	# push the return value
+	push	%rax
+	# generate code for the right operand
+	# push the integer
+	mov	$5, %rax
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the multiplication
+	imul	%rbx, %rax
+	# push the expression result
+	push	%rax
+	# generate code for the right operand
+	# generate code for the left operand
+	# evaluate the parameter
+	mov	-24(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	go
+	# push the return value
+	push	%rax
+	# generate code for the right operand
+	# evaluate the parameter
+	mov	-8(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	go
+	# push the return value
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the division
+	cdq
+	idiv	%rbx
+	# push the expression result
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the addition
+	add	%rbx, %rax
+	# push the expression result
+	push	%rax
+	pop	%rax
+	mov	%rax, -24(%rbp)
+	# generate code for the right-hand side of the assignment
+	# generate code for the left operand
+	# generate code for the left operand
+	# generate code for the left operand
+	# push the integer
+	mov	$1, %rax
+	push	%rax
+	# generate code for the right operand
+	# evaluate the parameter
+	# push the integer
+	mov	$1, %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	fi
+	# push the return value
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the addition
+	add	%rbx, %rax
+	# push the expression result
+	push	%rax
+	# generate code for the right operand
+	# generate code for the left operand
+	# evaluate the parameter
+	# push the integer
+	mov	$2, %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	do
+	# push the return value
+	push	%rax
+	# generate code for the right operand
+	# push the integer
+	mov	$10, %rax
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the division
+	cdq
+	idiv	%rbx
+	# push the expression result
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the subtraction
+	sub	%rbx, %rax
+	# push the expression result
+	push	%rax
+	# generate code for the right operand
+	# generate code for the left operand
+	# generate code for the left operand
+	# evaluate the parameter
+	mov	-16(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	go
+	# push the return value
+	push	%rax
+	# generate code for the right operand
+	# push the integer
+	mov	$5, %rax
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the multiplication
+	imul	%rbx, %rax
+	# push the expression result
+	push	%rax
+	# generate code for the right operand
+	# generate code for the left operand
+	# evaluate the parameter
+	mov	-24(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	go
+	# push the return value
+	push	%rax
+	# generate code for the right operand
+	# evaluate the parameter
+	mov	-8(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	go
+	# push the return value
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the division
+	cdq
+	idiv	%rbx
+	# push the expression result
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the addition
+	add	%rbx, %rax
+	# push the expression result
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the remainder
+	cdq
+	idiv	%rbx
+	mov	%rdx, %rax
+	# push the expression result
+	push	%rax
+	pop	%rax
+	mov	%rax, -32(%rbp)
+	# generate code for the right-hand side of the assignment
+	# evaluate the parameter
+	mov	-16(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	no
+	# push the return value
+	push	%rax
+	pop	%rax
+	mov	%rax, -16(%rbp)
+	# generate code for the right-hand side of the assignment
+	# generate code for the left operand
+	# evaluate the parameter
+	# push the integer
+	mov	$2, %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	do
+	# push the return value
+	push	%rax
+	# generate code for the right operand
+	mov	-16(%rbp), %rax
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the multiplication
+	imul	%rbx, %rax
+	# push the expression result
+	push	%rax
+	pop	%rax
+	mov	%rax, -24(%rbp)
+	# generate code for the right-hand side of the assignment
+	# push the integer
+	mov	$777, %rax
+	push	%rax
+	pop	%rax
+	mov	%rax, -32(%rbp)
+	# generate code for the right-hand side of the assignment
+	# generate code for the left operand
+	mov	-32(%rbp), %rax
+	push	%rax
+	# generate code for the right operand
+	# evaluate the parameter
+	mov	-32(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	go
+	# push the return value
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the remainder
+	cdq
+	idiv	%rbx
+	mov	%rdx, %rax
+	# push the expression result
+	push	%rax
+	pop	%rax
+	mov	%rax, -32(%rbp)
 	# generate code for the return expression
 	# push the integer
 	mov	$5, %rax
