b2f16b27b5ce26db6061148ac57e977c30009170
kaisil <kaisil@protonmail.com>
2020-11-17 14:59:17 -0500

    Update factorial.s

--- proj3/factorial.s
+++ proj3/factorial.s
@@ -6,13 +6,30 @@ factorial:
 	# emit the function prologue
 	push	%rbp
 	mov	%rsp, %rbp
-	sub	$16, %rsp
+	sub	$8, %rsp
 	push	%rbx
 	# move parameter onto the stack
 	mov	%rdi, -8(%rbp)
 	# generate code for the body
+	# generate code for the right-hand side of the assignment
+	# generate code for the left operand
+	mov	-8(%rbp), %rax
+	push	%rax
+	# generate code for the right operand
+	mov	-8(%rbp), %rax
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the multiplication
+	imul	%rbx, %rax
+	# push the expression result
+	push	%rax
+	pop	%rax
+	mov	%rax, -8(%rbp)
 	# generate code for the return expression
-	mov	-16(%rbp), %rax
+	mov	-8(%rbp), %rax
 	push	%rax
 	# save the return expression into %rax per the abi
 	pop	%rax
@@ -37,10 +54,94 @@ main:
 	# generate code for the body
 	# generate code for the right-hand side of the assignment
 	# push the integer
-	mov	$4, %rax
+	mov	$2, %rax
 	push	%rax
 	pop	%rax
 	mov	%rax, -8(%rbp)
+	# generate code for the right-hand side of the assignment
+	# evaluate the parameter
+	mov	-8(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	factorial
+	# push the return value
+	push	%rax
+	pop	%rax
+	mov	%rax, -16(%rbp)
+	# generate code for the right-hand side of the assignment
+	# evaluate the parameter
+	mov	-8(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	factorial
+	# push the return value
+	push	%rax
+	pop	%rax
+	mov	%rax, -16(%rbp)
+	# generate code for the right-hand side of the assignment
+	# evaluate the parameter
+	mov	-8(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	factorial
+	# push the return value
+	push	%rax
+	pop	%rax
+	mov	%rax, -16(%rbp)
+	# generate code for the right-hand side of the assignment
+	# evaluate the parameter
+	mov	-8(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	factorial
+	# push the return value
+	push	%rax
+	pop	%rax
+	mov	%rax, -16(%rbp)
+	# generate code for the right-hand side of the assignment
+	# evaluate the parameter
+	mov	-8(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	factorial
+	# push the return value
+	push	%rax
+	pop	%rax
+	mov	%rax, -16(%rbp)
+	# generate code for the right-hand side of the assignment
+	# evaluate the parameter
+	mov	-8(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	factorial
+	# push the return value
+	push	%rax
+	pop	%rax
+	mov	%rax, -16(%rbp)
+	# generate code for the right-hand side of the assignment
+	# evaluate the parameter
+	mov	-8(%rbp), %rax
+	push	%rax
+	# pass the parameter
+	pop	%rdi
+	# call the function
+	call	factorial
+	# push the return value
+	push	%rax
+	pop	%rax
+	mov	%rax, -16(%rbp)
 	# generate code for the return expression
 	# push the integer
 	mov	$7, %rax
