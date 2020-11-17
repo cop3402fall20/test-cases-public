b08c6277852437dcfe5673e60ed104646ab51b78
kaisil <kaisil@protonmail.com>
2020-11-17 14:10:10 -0500

    Update to match

--- proj3/divide.s
+++ proj3/divide.s
@@ -1,8 +1,8 @@
 .file "stdin"
 .text
-.globl convert
-.type convert, @function
-convert:
+.globl divide
+.type divide, @function
+divide:
 	# emit the function prologue
 	push	%rbp
 	mov	%rsp, %rbp
@@ -11,6 +11,65 @@ convert:
 	# move parameter onto the stack
 	mov	%rdi, -8(%rbp)
 	# generate code for the body
+	# generate code for the right-hand side of the assignment
+	# generate code for the left operand
+	# generate code for the left operand
+	# generate code for the left operand
+	mov	-8(%rbp), %rax
+	push	%rax
+	# generate code for the right operand
+	# push the integer
+	mov	$2, %rax
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the remainder
+	cdq
+	idiv	%rbx
+	mov	%rdx, %rax
+	# push the expression result
+	push	%rax
+	# generate code for the right operand
+	# push the integer
+	mov	$10, %rax
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the addition
+	add	%rbx, %rax
+	# push the expression result
+	push	%rax
+	# generate code for the right operand
+	# generate code for the left operand
+	mov	-8(%rbp), %rax
+	push	%rax
+	# generate code for the right operand
+	# push the integer
+	mov	$2, %rax
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the division
+	cdq
+	idiv	%rbx
+	# push the expression result
+	push	%rax
+	# pop the right operand
+	pop	%rbx
+	# pop the left operand
+	pop	%rax
+	# do the multiplication
+	imul	%rbx, %rax
+	# push the expression result
+	push	%rax
+	pop	%rax
+	mov	%rax, -16(%rbp)
 	# generate code for the return expression
 	mov	-16(%rbp), %rax
 	push	%rax
@@ -48,7 +107,7 @@ main:
 	# pass the parameter
 	pop	%rdi
 	# call the function
-	call	convert
+	call	divide
 	# push the return value
 	push	%rax
 	pop	%rax
