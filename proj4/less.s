09e6a63fc230144cc872e3880303708da084ef2d
kaisil <kaisil@protonmail.com>
2020-11-28 18:25:27 -0500

    Removed ">" and ">="

---
+++ proj4/less.s
@@ -0,0 +1,43 @@
+.file "stdin"
+.text
+.globl main
+.type main, @function
+main:
+	# stack space for argc and argv
+	# emit main's prologue
+	push	%rbp
+	mov	%rsp, %rbp
+	sub	$40, %rsp
+	push	%rbx
+	# move argc and argv from parameter registers to the stack
+	mov	%rdi, -32(%rbp)
+	mov	%rsi, -40(%rbp)
+	# generate code for the body
+	# generate code for the right-hand side of the assignment
+	# push the integer
+	mov	$2, %rax
+	push	%rax
+	pop	%rax
+	mov	%rax, -8(%rbp)
+	# generate code for the right-hand side of the assignment
+	# push the integer
+	mov	$1, %rax
+	push	%rax
+	pop	%rax
+	mov	%rax, -16(%rbp)
+	# generate code for the right-hand side of the assignment
+	# push the integer
+	mov	$0, %rax
+	push	%rax
+	pop	%rax
+	mov	%rax, -24(%rbp)
+	# generate code for the return expression
+	mov	-8(%rbp), %rax
+	push	%rax
+	# save the return expression into %rax per the abi
+	pop	%rax
+	# emit main's epilogue
+	pop	%rbx
+	mov	%rbp, %rsp
+	pop	%rbp
+	ret
